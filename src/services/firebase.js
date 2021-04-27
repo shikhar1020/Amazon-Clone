a9ab4a24b407c095c85d25a41913f8f6910035c2
Shikhar Srivastava <54438024+shikhar1020@users.noreply.github.com>
2021-04-27 22:33:16 +0530

    register & login via email and password

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,10 +1,19 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAmqF5KtrbXw_ARPeNjVspm7ax2KmYqa68",
-    authDomain: "clone-4277b.firebaseapp.com",
-    projectId: "clone-4277b",
-    storageBucket: "clone-4277b.appspot.com",
-    messagingSenderId: "289417026213",
-    appId: "1:289417026213:web:04e41951fff885d2bd8b4c",
-    measurementId: "G-GTYD0MPZSL"
-  };
+  apiKey: "AIzaSyAmqF5KtrbXw_ARPeNjVspm7ax2KmYqa68",
+  authDomain: "clone-4277b.firebaseapp.com",
+  projectId: "clone-4277b",
+  storageBucket: "clone-4277b.appspot.com",
+  messagingSenderId: "289417026213",
+  appId: "1:289417026213:web:04e41951fff885d2bd8b4c",
+  measurementId: "G-GTYD0MPZSL",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore(); //databse of firebase
+const auth = firebase.auth(); //authintication via firebase
+
+export { db, auth };
